bba81a703f31178e963f03c47a0a44798c898039
100911 <100911@001SIS0407>
2019-11-01 12:56:10 -0300

    init

---
+++ criptografiaservidor/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criptografiaservidor
+{
+    public class Server
+    {
+        static void Main()
+        {
+            Arquivo();
+        }
+
+
+        public static void Arquivo()
+        {
+            //string text = System.IO.File.ReadAllText(@"C:\Felipe\Teste.txt");
+
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Felipe\Teste.txt");
+
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
+
