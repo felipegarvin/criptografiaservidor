ee264416f00603ec720a2eb59690b453cbfd8d61
100911 <100911@001SIS0407>
2019-11-01 13:17:23 -0300

    init

--- criptografiaservidor/Server.cs
+++ criptografiaservidor/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace criptografiaservidor
 {
@@ -16,21 +17,57 @@ namespace criptografiaservidor
 
         public static void Arquivo()
         {
-            //string text = System.IO.File.ReadAllText(@"C:\Felipe\Teste.txt");
-
-            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
 
             string[] lines = System.IO.File.ReadAllLines(@"C:\Felipe\Teste.txt");
 
-            System.Console.WriteLine("Contents of WriteLines2.txt = ");
-            foreach (string line in lines)
+            string operador;
+            string val1;
+            string val2;
+            double result;
+
+            val1 = Convert.ToString(lines[0]);
+            operador = Convert.ToString(lines[1]);
+            val2 = Convert.ToString(lines[2]);
+            
+            result = Calcular(operador, val1, val2);
+
+            string nomeArquivo = @"C:\Felipe\Teste2.txt";
+
+            StreamWriter writer = new StreamWriter(nomeArquivo);
+
+            writer.WriteLine(result);
+
+            writer.Close();
+
+        }
+        public static double Calcular(string operador, string val1, string val2)
+        {
+            double result = 0;
+
+            if (operador == "+")
             {
-                Console.WriteLine("\t" + line);
+                result = Convert.ToDouble(val1) + Convert.ToDouble(val2);
+            }
+            else if (operador == "-")
+            {
+                result = Convert.ToDouble(val1) - Convert.ToDouble(val2);
+            }
+            else if (operador == "*")
+            {
+                result = Convert.ToDouble(val1) * Convert.ToDouble(val2);
+            }
+            else if (operador == "/")
+            {
+                result = Convert.ToDouble(val1) / Convert.ToDouble(val2);
+            }
+            else
+            {
+                result = 0;
             }
 
-            Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+            return result;
         }
-    }
+
+        }
 }
 
